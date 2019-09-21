23915b0a3961ceefa58f1fe840361e97aed7229a
Sharon Gold <sharongo113@gmail.com>
2019-09-21 11:21:32 +0300

    add alert

---
+++ client/src/pages/Home.js
@@ -0,0 +1,28 @@
+import React, { useContext, useEffect } from 'react';
+import Contacts from '../components/contacts/Contacts';
+import ContactForm from '../components/contacts/ContactForm';
+import ContactFilter from '../components/contacts/ContactFilter';
+import AuthContext from '../context/auth/authContext';
+
+const Home = () => {
+    const authContext = useContext(AuthContext);
+
+    useEffect(() => {
+        authContext.loadUser();
+        // eslint-disable-next-line
+    }, []);
+
+    return (
+        <div className='grid-2'>
+            <div>
+                <ContactForm />
+            </div>
+            <div>
+                <ContactFilter />
+                <Contacts />
+            </div>
+        </div>
+    );
+};
+
+export default Home;
